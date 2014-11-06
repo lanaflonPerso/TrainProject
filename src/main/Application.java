badec45593b7c938400993b26795962888bd1d4f
Vadim Pechenoga <vad.pechenoga@gmail.com>
2014-11-06 20:19:53 +0200

    ActionListener for for Exit JMenuItem

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,23 +1,74 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Light on 05.11.2014.
  */
 public class Application {
+    JPanel mainPanel;
+    JPanel mapPanel;
+    JPanel elementsPanel;
+    JPanel trainLogPanel;
+    JPanel commonLogPanel;
+
     JFrame frame;
-    JPanel panel;
+    BufferedImage mapImage;
 
 
     public Application() {
         frame = new JFrame("TrainProject");
-        panel = new JPanel();
+        mainPanel = new JPanel();
+        mapPanel = new JPanel();
+        mapPanel = new JPanel();
+        elementsPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        elementsPanel.setLayout(new GridLayout(3,3));
+        elementsPanel.setBackground(Color.YELLOW);
+        GridBagConstraints c = new GridBagConstraints();
 
         // main menu
         setMenuBar();
-        // git request from IDE
-        frame.add(panel);
+
+        // map
+        try {
+            mapImage = ImageIO.read(new File("C:\\Users\\Light\\IdeaProjects\\TrainProject\\img\\common.png"));
+        } catch (IOException e) {
+            System.out.println("Error. Invalid path to map image.");
+        }
+        JLabel mapLabel = new JLabel(new ImageIcon(mapImage));
+
+        mapPanel.add(mapLabel);
+        c.gridx = 0;
+        c.gridy = 0;
+//        c.insets = new Insets(0, 0, 0, 200);
+        mainPanel.add(mapPanel, c);
+
+        // emements
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        elementsPanel.add(new JLabel("Test"));
+        c.gridx = 1;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(0, 0, 0, 0);
+        c.anchor = GridBagConstraints.NORTH;
+//        mainPanel.add(elementsPanel, c);
+
+        frame.add(mainPanel);
         frame.setBounds(130,16,1100,700);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -49,6 +100,14 @@ public class Application {
         myMenu.addSeparator();
         myItem = new JMenuItem("Вихід");
         myMenu.add(myItem);
+        myItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                int exitResult = JOptionPane.showConfirmDialog(null, "Ви впевнені, що хочете завершити роботу програми?", "Вихід з програми", JOptionPane.ERROR_MESSAGE);
+                if (exitResult == 0) {
+                    System.exit(0);
+                }
+            }
+        });
         return myMenu;
     }
 
