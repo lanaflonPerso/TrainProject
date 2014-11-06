3b68ff82a247c2627ddfc65e7f791f1b667cb3de
Vadim Pechenoga <vad.pechenoga@gmail.com>
2014-11-06 21:05:15 +0200

    New structure of GUI

--- src/main/Application.java
+++ src/main/Application.java
@@ -16,8 +16,7 @@ public class Application {
     JPanel mainPanel;
     JPanel mapPanel;
     JPanel elementsPanel;
-    JPanel trainLogPanel;
-    JPanel commonLogPanel;
+    JPanel interruptsPanel;
 
     JFrame frame;
     BufferedImage mapImage;
@@ -27,31 +26,34 @@ public class Application {
         frame = new JFrame("TrainProject");
         mainPanel = new JPanel();
         mapPanel = new JPanel();
-        mapPanel = new JPanel();
         elementsPanel = new JPanel();
-        mainPanel.setLayout(new GridBagLayout());
-        elementsPanel.setLayout(new GridLayout(3,3));
-        elementsPanel.setBackground(Color.YELLOW);
-        GridBagConstraints c = new GridBagConstraints();
+        interruptsPanel = new JPanel();
+
+        mainPanel.setLayout(null);
+        mapPanel.setBounds(0, 0, Map.WIDTH, Map.HEIGHT);
+        elementsPanel.setBounds(Map.WIDTH + 20, 0, 260, Map.HEIGHT);
+        interruptsPanel.setBounds(0, Map.HEIGHT + 5, 880, 40);
+
+        elementsPanel.setLayout(new GridLayout(5,2));
+        // temporarily settings
+        mapPanel.setBackground(Color.YELLOW);
+        elementsPanel.setBackground(Color.GREEN);
+        interruptsPanel.setBackground(Color.BLUE);
 
         // main menu
         setMenuBar();
 
         // map
         try {
-            mapImage = ImageIO.read(new File("C:\\Users\\Light\\IdeaProjects\\TrainProject\\img\\common.png"));
+            mapImage = ImageIO.read(new File("C:\\Users\\Light\\IdeaProjects\\TrainProject\\img\\map.png"));
         } catch (IOException e) {
             System.out.println("Error. Invalid path to map image.");
         }
         JLabel mapLabel = new JLabel(new ImageIcon(mapImage));
-
+        mapPanel.setBackground(Color.ORANGE);
         mapPanel.add(mapLabel);
-        c.gridx = 0;
-        c.gridy = 0;
-//        c.insets = new Insets(0, 0, 0, 200);
-        mainPanel.add(mapPanel, c);
 
-        // emements
+        // elements
         elementsPanel.add(new JLabel("Test"));
         elementsPanel.add(new JLabel("Test"));
         elementsPanel.add(new JLabel("Test"));
@@ -61,15 +63,14 @@ public class Application {
         elementsPanel.add(new JLabel("Test"));
         elementsPanel.add(new JLabel("Test"));
         elementsPanel.add(new JLabel("Test"));
-        c.gridx = 1;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.insets = new Insets(0, 0, 0, 0);
-        c.anchor = GridBagConstraints.NORTH;
-//        mainPanel.add(elementsPanel, c);
+
+        // add all parts to main Panel
+        mainPanel.add(mapPanel);
+        mainPanel.add(elementsPanel);
+        mainPanel.add(interruptsPanel);
 
         frame.add(mainPanel);
-        frame.setBounds(130,16,1100,700);
+        frame.setBounds(225,16,900,700);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setVisible(true);
@@ -88,7 +89,7 @@ public class Application {
 
     private JMenu getMenu() {
         JMenu myMenu = new JMenu("Меню");
-        JMenuItem myItem = new JMenuItem("Пункт 1");
+        JMenuItem myItem = new JMenuItem("Пункт 1                    ");
         myMenu.add(myItem);
         myItem = new JMenuItem("Пункт 2");
 //        myItem.setEnabled(false);
@@ -113,7 +114,7 @@ public class Application {
 
     private JMenu getTranslation() {
         JMenu myMenu = new JMenu("Трансляція");
-        JMenuItem myItem = new JMenuItem("Пункт 1");
+        JMenuItem myItem = new JMenuItem("Пункт 1          ");
         myMenu.add(myItem);
         myItem = new JMenuItem("Пункт 2");
         myMenu.add(myItem);
---
+++ src/main/Map.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Light on 06.11.2014.
+ */
+public class Map {
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 600;
+
+}
