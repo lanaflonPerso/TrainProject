50055c0f85d05af33ba7652645659fa98528b18a
Vadim Pechenoga <vad.pechenoga@gmail.com>
2014-11-06 03:16:25 +0200

    Main Window of app with menu bar

---
+++ src/main/Application.java
@@ -0,0 +1,69 @@
+package main;
+
+import javax.swing.*;
+
+/**
+ * Created by Light on 05.11.2014.
+ */
+public class Application {
+    JFrame frame;
+    JPanel panel;
+
+
+    public Application() {
+        frame = new JFrame("TrainProject");
+        panel = new JPanel();
+
+        // main menu
+        setMenuBar();
+
+        frame.add(panel);
+        frame.setBounds(130,16,1100,700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    private void setMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuItem = getMenu();
+        menuBar.add(menuItem);
+        menuItem = getTranslation();
+        menuBar.add(menuItem);
+        menuItem = new JMenu("Пуск");
+        menuBar.add(menuItem);
+        frame.setJMenuBar(menuBar);
+    }
+
+    private JMenu getMenu() {
+        JMenu myMenu = new JMenu("Меню");
+        JMenuItem myItem = new JMenuItem("Пункт 1");
+        myMenu.add(myItem);
+        myItem = new JMenuItem("Пункт 2");
+//        myItem.setEnabled(false);
+        myMenu.add(myItem);
+        myItem = new JMenuItem("Пункт 3");
+        myMenu.add(myItem);
+        myItem = new JMenuItem("Пункт 4");
+        myMenu.add(myItem);
+        myMenu.addSeparator();
+        myItem = new JMenuItem("Вихід");
+        myMenu.add(myItem);
+        return myMenu;
+    }
+
+    private JMenu getTranslation() {
+        JMenu myMenu = new JMenu("Трансляція");
+        JMenuItem myItem = new JMenuItem("Пункт 1");
+        myMenu.add(myItem);
+        myItem = new JMenuItem("Пункт 2");
+        myMenu.add(myItem);
+        myItem = new JMenuItem("Пункт 3");
+        myMenu.add(myItem);
+        return myMenu;
+    }
+
+    public static void main(String[] args) {
+        Application app = new Application();
+    }
+}
