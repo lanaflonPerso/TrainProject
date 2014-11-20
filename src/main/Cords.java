8ffcb3454a4dc60eb0af53d957c8e703ecae9609
Vadim Pechenoga <vad.pechenoga@gmail.com>
2014-11-20 19:40:08 +0200

    Coordinates class

---
+++ src/main/Cords.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Coordinates class
+ */
+public class Cords {
+    int x;
+    int y;
+
+    public Cords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
