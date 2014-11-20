ba9bf2e470b7d72b922c1e5bbd583a1723f82444
Vadim Pechenoga <vad.pechenoga@gmail.com>
2014-11-20 13:16:29 +0200

    Change map

--- src/main/Map.java
+++ src/main/Map.java
@@ -4,7 +4,7 @@ package main;
  * Created by Light on 06.11.2014.
  */
 public class Map {
-    public static final int WIDTH = 600;
-    public static final int HEIGHT = 600;
+    public static final int WIDTH = 761;
+    public static final int HEIGHT = 641;
 
 }
